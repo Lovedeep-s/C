8638799c228beab16714b21108d717874253e0e0
Lovedeep Singh <s.lovedeepparmarpta@gmail.com>
2022-03-21 15:04:58 +0530

    initial commit

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,67 @@
 #include<iostream>
 using namespace std;
+int a[10];
+int rear=0,front=0;
+void nq();
+void dq();
+void print();
+
 int main()
 {
-    cout<<"hello";
+    int n=0;
+    while(n!=4)
+    {
+    cout<<"enter your choice";
+    cin>>n;   
+    switch (n)
+    {
+    case 1:dq(); break;
+    case 2:nq(); break;
+    case 3:print(); break;
+    case 4:exit; 
+    default:exit;
+    }
+    }
+}
+void nq()
+{
+    int i,j;
+    cout<<"enter  of element";
+    cin>>i;
+    if(rear>5)
+    cout<<"Overflow";
+    else
+    {
+        for ( j = 0; j < i;j++)
+        {
+            cin>>a[ j ];
+            rear++;
+
+        }
+    }
+}
+void print()
+ {
+     int i=front;
+        for(; i < rear; i++)
+    {
+        cout<<a[i];
+    }
+    
+}
+void dq()
+{
+    int i,j;
+    cout<<"enter  of element";
+    cin>>i;
+    if(front > rear)
+    cout<<"under flow";
+    else
+    {
+        for ( j = 0; j < i;j++)
+        {
+            front++;
+
+        }
+    }
 }
---
+++ input.py
@@ -0,0 +1,2 @@
+a= int(input("hi "))
+print(type(a))
